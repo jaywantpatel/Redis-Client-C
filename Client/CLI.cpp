fbc68fdcc1f7a42fec415ab859f9294bebf1deb0
jaywantpatel <56215664+jaywantpatel@users.noreply.github.com>
2025-11-25 14:16:21 -0500

    Update CLI.cpp

--- Client/CLI.cpp
+++ Client/CLI.cpp
@@ -9,17 +9,48 @@ static std::string trim(const std::string &s){
     return s.substr(start, end-start+1);
 }
 
-CLI::CLI(const std::string &host, int port) 
-    : redisClient(host, port) {}
+void printHelp() {
+    std::cout << "my_redis_cli 1.0.0\n"
+              << "Usage: \n"
+              << "      With arguments:            ./my_redis_cli -h <host> -p <port>\n"
+              << "      Default Host (127.0.0.1):  ./my_redis_cli -p <port>\n"
+              << "      Default Port (6379):       ./my_redis_cli -h <host>\n"
+              << "      One-shot execution:        ./my_redis_cli <command> [arguments]\n"
+              << "\n"
+              << "Interactive Mode (REPL):\n"
+              << "      ./my_redis_cli\n"
+              << "      Type Redis commands directly.\n"
+              << "\n"
+              << "To get help about Redis commands type:\n"
+              << "      \"help\" to display this help message\n"
+              << "      \"quit\" to exit\n"
+              << "\n"
+              << "Examples:\n"
+              << "      ./my_redis_cli PING\n"
+              << "      ./my_redis_cli SET mykey \"Hello World\"\n"
+              << "      ./my_redis_cli GET mykey\n"
+              << "\n"
+              << "To set my_redis_cli preferences:\n"
+              << "      \":set hints\" enable online hints\n"
+              << "      \":set nohints\" disable online hints\n"
+              << "Set your preferences in ~/.myredisclirc\n"
+              << std::endl;
+}
 
-void CLI::run() {
+CLI::CLI(const std::string &host, int port) 
+    : host(host), port(port), redisClient(host, port) {}
+
+void CLI::run(const std::vector<std::string>& commandArgs) {
    if(!redisClient.connectToServer()){
         return; 
    }
 
+   if(!commandArgs.empty()){
+        executeCommand(commandArgs);
+        return;
+   }
+   
    std::cout << "Connected to Redis at : " << redisClient.getSocketFD() << "\n";
-   std::string host = "127.0.0.1";
-   int port = 6379;
 
    while (true) {
         std::cout<< host << ":" << port << "> ";
@@ -28,13 +59,13 @@ void CLI::run() {
         if (!std::getline(std::cin, line)) break;
         line = trim(line);
         if (line.empty()) continue;
-        if (line == "quit") {
+        if (line == "quit" || line == "exit") {
             std::cout<< "Goodbye. \n";
             break;
         }
 
         if (line == "help"){
-            std::cout << "Displaying help\n";
+            printHelp();
             continue;
         }
 
@@ -56,4 +87,18 @@ void CLI::run() {
         std::cout << response << "\n";
    }
    redisClient.disconnect();
+}
+
+void CLI::executeCommand(const std::vector<std::string>& args){
+    if (args.empty()) return;
+
+    std::string command = CommandHandler::buildRESPcommand(args);
+    if ( !redisClient.sendCommand(command)){
+        std::cerr << "(Error) Failed to send command. \n";
+        return;
+    }
+
+    //Parse and print the response
+    std::string response = ResponseParser::parseResponse(redisClient.getSocketFD());
+    std::cout << response << "\n";
 }
