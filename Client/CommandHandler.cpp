a484f5894335e2a7c3b63b1ed971639baea04ca2
jaywantpatel <56215664+jaywantpatel@users.noreply.github.com>
2025-12-17 18:13:06 -0500

    Update CommandHandler.cpp

--- Client/CommandHandler.cpp
+++ Client/CommandHandler.cpp
@@ -1,39 +1,37 @@
 #include "CommandHandler.h"
 #include <regex>
+#include <sstream>
 
-
-std::vector<std::string> CommandHandler::splitArgs(const std::string &input){
+std::vector<std::string> CommandHandler::splitArgs(const std::string &input) {
     std::vector<std::string> tokens;
 
-    //Regex to match words or quoted strings 
-    std::regex rgx(R"((\"[^\"]+\"|\S+))"); // command \"arg1 with spaces\" arg2 \"arg3\"
-    auto words_beign = std::sregex_iterator(input.begin(), input.end(), rgx);
+    // Regex to match words or quoted strings correctly
+    std::regex rgx(R"((\"[^\"]+\"|\S+))"); 
+    auto words_begin = std::sregex_iterator(input.begin(), input.end(), rgx);
     auto words_end = std::sregex_iterator();
 
-    for(auto it = words_beign; it != words_end; ++it){
+    for (auto it = words_begin; it != words_end; ++it) {
         std::string token = it->str();
-        // Remove quotes 
-        if (token.size() >= 2 && token.front() == '\"' && token.back() == '\"' ){
-            token = token.substr(1,token.size()-2);
+        // Remove quotes
+        if (token.size() >= 2 && token.front() == '\"' && token.back() == '\"') {
+            token = token.substr(1, token.size() - 2);
         }
         tokens.push_back(token);
     }
 
     return tokens;
 }
-
 /*
 * -> start of an array
 $ -> bulk of string
 +arg
 */
-std::string CommandHandler::buildRESPcommand(const std::vector<std::string> &args){
+std::string CommandHandler::buildRESPcommand(const std::vector<std::string> &args) {
     std::ostringstream oss;
-    oss << "*" << args.size() << "\r\n"; //num of args
+    oss << "*" << args.size() << "\r\n"; // num of args
 
-    for(const auto &arg : args){
-        oss << "$" << arg.size() << "\r\n" << arg << "\r\n"; //len and value of the arg
+    for (const auto &arg : args) {
+        oss << "$" << arg.size() << "\r\n" << arg << "\r\n"; // len and value of arg
     }
-
     return oss.str();
 }
