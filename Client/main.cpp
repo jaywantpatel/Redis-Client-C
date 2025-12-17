c3788c1a2d8748b3fc1f12c2f128497bc5f65401
jaywantpatel <56215664+jaywantpatel@users.noreply.github.com>
2025-12-17 18:15:24 -0500

    Update main.cpp

--- Client/main.cpp
+++ Client/main.cpp
@@ -1,17 +1,17 @@
 /*
 1. Command-Line argument parsing
-    -h <host> default: 127.0.0.1 -p <port> default: 6379
+    -h <host>  default: 127.0.0.1, -p <port> default: 6379
     If no args, launch interactive REPL mode
 
 2. Object-Oriented Programming
-    RedisClient, CommandHandler, ResponseParser, CLI
+    RedisClient, CommandHandler, RepsonseParser, CLI
 
 3. Establish TCP Connection to Redis (RedisClient)
     Berkeley sockets to open TCP connection
-    IPv4 and IPv6 `getaddrinfo`
+    IPv4 and Ipv6 `getaddrinfo`
 
 4. Parsing and Command Formatting (CommandHandler)
-    Split user input 
+    Split user input
     Convert commands into RESP format:
     ```
     *3\r\n
@@ -19,20 +19,20 @@
     $3\r\nkey\r\n
     $5\r\nvalue\r\n
     ```
-5. Handle Redis Responses (ResponseParser)
+5. Handlig Redis Responses (RepsonseParser)
     Read server responses and parses RESP types
-    +OK, -Error, :100, $5\r\nhello -> Bulk string, *2\r\n$3\r\nfoo\r\n$3\r\nbar -> Array response
+    +OK, -Error, :100, $5\r\nhello → Bulk string, *2\r\n$3\r\nfoo\r\n$3\r\nbar → Array response
 
 6. Implement Interactive REPL (CLI)
-    Run loop: User Input, valid redis commands, send commands to server, display parsed response
+    Run loop: User Input, valis redis commands, send commandsd to the server, display parsed response
     Support: help, quit
 
-7. main.cpp: parse command-line arguments, instantiate cli and launch in REPL mode.
+7. main.cpp: parse command-line args, instantiate CLI and launc in REPL mode.
 
 Socket Programming
-Protocol Handeling
-OOP principles 
-CLI development
+Protocol Handleing
+OOP princaples
+CLI developement
 
 */
 
@@ -46,17 +46,16 @@ int main(int argc, char* argv[]) {
     int i = 1;
     std::vector<std::string> commandArgs;
 
-    //Parse command-line args for -h and -p
-    while(i < argc){
+    // Parse command-line args for -h and -p
+    while (i < argc) {
         std::string arg = argv[i];
-        if (arg == "-h" && i+1 < argc){ // -h 127.0.0.1
+        if (arg == "-h" && i + 1 < argc) {// -h 127.0.0.1
             host = argv[++i];
-        } else if (arg == "-p" && i+1 < argc){
+        } else if (arg == "-p" && i + 1 < argc) {
             port = std::stoi(argv[++i]);
         } else {
-            // Remaining arguments
-            while (i < argc)
-            {
+            // Remaining args
+            while (i <argc) {
                 commandArgs.push_back(argv[i]);
                 i++;
             }
@@ -65,10 +64,9 @@ int main(int argc, char* argv[]) {
         ++i;
     }
 
-    //Handle REPL and one-shot command modes
+    // Handle REPL and one-shot command modes
     CLI cli(host, port);
-    cli.run(commandArgs); //launch REPL mode
-    
+    cli.run(commandArgs);
 
     return 0;
 }
